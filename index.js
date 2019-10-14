f537b8c95aa76ff9b235be72cbf36f3ff2366205
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-14 16:57:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {};
+
+var updateObjectWithKeyAndValue = (object, key, value) => {
+  var object = Object.assign(object, {[key]: value})
+  return object;
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+  object.key = value;
+  return object
+}
